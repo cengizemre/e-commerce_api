a0c73a0f3f0699a2b4a34ce5e4515c84651854b1
Emre Cengiz <emrec759@gmail.com>
2021-10-22 15:21:50 +0300

    created models

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => { console.log("db connected") }).catch((err) => { console.log("Error:" + err) });
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => { console.log("db connected") })
+    .catch((err) => {
+        console.log("Error:" + err)
+    });
 
+//Routes
+app.use('/api/', userRoute);
 
 
 app.listen(process.env.PORT, (req, res) => {
---
+++ models/Order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    products: [
+        {
+            productId: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
+    amount: { type: String, required: true },
+    adress: { type: String, required: true },
+    status: { type: String, defalut: 'gönderiliyor' }
+},
+    { timestamps: true }
+);
+module.exports = mongoose.model("Order", OrderSchema);
