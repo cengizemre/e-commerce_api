45aded46d8fa00da15831284ea0742f5b363aee8
Emre Cengiz <emrec759@gmail.com>
2021-10-23 13:48:37 +0300

    User routes, verifyToken and Auth finished.

--- routes/authentication.js
+++ routes/authentication.js
@@ -34,7 +34,7 @@ router.post('/login', async (req, res) => {
             isAdmin: user.isAdmin
         },
             process.env.JWT_SECRET,
-            { expiresIn: "90d" }
+            { expiresIn: "3d" }
         )
 
         const { password, ...others } = user._doc;
