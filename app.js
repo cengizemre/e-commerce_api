907a937e93f442f6ce932ff2cbae6170f202e990
Emre Cengiz <emrec759@gmail.com>
2021-10-24 12:20:17 +0300

    stripe api added.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const authRoute = require('./routes/authentication');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require("./routes/stripe");
 app.use(express.json());
 
 //database connect
@@ -22,6 +23,7 @@ app.use('/api/', authRoute);
 app.use('/api/', userRoute);
 app.use('/api/', cartRoute);
 app.use('/api/', orderRoute);
+app.use('/api/', stripeRoute);
 app.listen(process.env.PORT, (req, res) => {
     console.log(`server listening on ${process.env.PORT}`)
 });
